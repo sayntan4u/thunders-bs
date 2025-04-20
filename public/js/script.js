9b35ef2f51dfde31500449f64db8c4c761b4e9b9
Sayantan <sayantan.tapadar9@gmail.com>
2025-04-20 21:43:28 +0530

    Fixed sidebar - UI CHANGE

--- public/js/script.js
+++ public/js/script.js
@@ -3,13 +3,17 @@ $(document).ready(function () {
     const sidebarState = localStorage.getItem('sidebarState');
     if (sidebarState === 'collapsed') {
         $('#sidebar').addClass('active');
+        $("#content").css("margin-left", "80px");
     }else{
         $('#sidebar').removeClass('active');
+        $("#content").css("margin-left", "250px");
     }
 
     // Sidebar toggle
     $('#sidebarCollapse').on('click', function () {
+        
         $('#sidebar').toggleClass('active');
+        $('#sidebar').hasClass('active') ?  $("#content").css("margin-left", "80px") :  $("#content").css("margin-left", "250px");
         // Save sidebar state to localStorage
         localStorage.setItem('sidebarState', $('#sidebar').hasClass('active') ? 'collapsed' : 'expanded');
     });
