f45d2745099aa25a28d3e731b0ea4cb3ca157057
Sayantan <sayantan.tapadar9@gmail.com>
2025-04-26 03:58:17 +0530

    chevron

--- public/js/agenda.js
+++ public/js/agenda.js
@@ -1,88 +1,136 @@
 Date.prototype.timeNow = function () {
-    return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
-}
+  return (
+    (this.getHours() < 10 ? "0" : "") +
+    this.getHours() +
+    ":" +
+    (this.getMinutes() < 10 ? "0" : "") +
+    this.getMinutes() +
+    ":" +
+    (this.getSeconds() < 10 ? "0" : "") +
+    this.getSeconds()
+  );
+};
 
 $("#btnAddAgenda").click(function () {
-    const task = $("#addAgendaTextBox").val();
-    const id = Date.now();
-    // console.log(id);
-    addAgendaFB(task, id);
-    addAgendaUI(task, id);
-    hideEditCard();
+  const task = $("#addAgendaTextBox").val();
+  const id = Date.now();
+  // console.log(id);
+  addAgendaFB(task, id);
+  addAgendaUI(task, id);
+  hideEditCard();
 });
 
-$("#deleteAgendaBtn").click(function(){
-    const id = $(this).parent().siblings(".modal-body").children("p").children(".task_id").html();
-    deleteAgendaFB(id);
-    deleteAgendaUI(id);
-    updateTaskCount("Delete");
+//chevron back
+$(".btn-back").click(function () {
+  const today = $(".date").html();
+  const yesterday = getYesterday(today);
+  $(".date").html(yesterday);
 });
 
-function deleteAgendaUI(id){
-    // console.log(id);
-    $(".tasks").children().each(function(){
-       if($(this).children(".task").children(".task_id").html() == id){
+//checvron forward
+$(".btn-forward").click(function () {
+  const today = $(".date").html();
+  const yesterday = getTommorrow(today);
+  $(".date").html(yesterday);
+});
+
+$("#deleteAgendaBtn").click(function () {
+  const id = $(this)
+    .parent()
+    .siblings(".modal-body")
+    .children("p")
+    .children(".task_id")
+    .html();
+  deleteAgendaFB(id);
+  deleteAgendaUI(id);
+  updateTaskCount("Delete");
+});
+
+function deleteAgendaUI(id) {
+  // console.log(id);
+  $(".tasks")
+    .children()
+    .each(function () {
+      if ($(this).children(".task").children(".task_id").html() == id) {
         $(this).remove();
-       }
+      }
     });
 }
 
-
 $(document).on("click", ".tasks .btn-checkbox", function () {
-    if ($(this).children(".material-icons").html() == "check_box") {
-        $(this).children(".material-icons").html("check_box_outline_blank");
-    }
-    else {
-        $(this).children(".material-icons").html("check_box");
-    }
-    updateTaskCount("Delete");
-    const dt = new Date();
-    updateConfirmAgendaFB($(this).siblings(".task_id").html(), dt.timeNow())
-    addCompletedNotification($(this).siblings(".task_id").html(), $(this).siblings("span").html(), dt.timeNow());
-    $(this).parent().parent().remove();
-
+  if ($(this).children(".material-icons").html() == "check_box") {
+    $(this).children(".material-icons").html("check_box_outline_blank");
+  } else {
+    $(this).children(".material-icons").html("check_box");
+  }
+  updateTaskCount("Delete");
+  const dt = new Date();
+  updateConfirmAgendaFB($(this).siblings(".task_id").html(), dt.timeNow());
+  addCompletedNotification(
+    $(this).siblings(".task_id").html(),
+    $(this).siblings("span").html(),
+    dt.timeNow()
+  );
+  $(this).parent().parent().remove();
 });
 
 $(document).on("click", ".tasks .btn-edit", function () {
-    const val = $(this).parent().siblings(".task_content").html();
-    $(this).parent().parent().siblings(".update-card").children(".card-body").children(".input-task").val(val);
+  const val = $(this).parent().siblings(".task_content").html();
+  $(this)
+    .parent()
+    .parent()
+    .siblings(".update-card")
+    .children(".card-body")
+    .children(".input-task")
+    .val(val);
 
-    $(this).parent().parent().addClass("hide");
-    $(this).parent().parent().siblings(".update-card").removeClass("hide");
-    $(this).parent().parent().siblings(".update-card").children(".card-body").children(".input-task").focus();
+  $(this).parent().parent().addClass("hide");
+  $(this).parent().parent().siblings(".update-card").removeClass("hide");
+  $(this)
+    .parent()
+    .parent()
+    .siblings(".update-card")
+    .children(".card-body")
+    .children(".input-task")
+    .focus();
 });
 
-
 $(document).on("click", ".completion_alerts .btn-undo", function () {
-    const id = $(this).parent().siblings(".task_id").html();
-    const task = $(this).parent().siblings("strong").children(".task_content").html();
+  const id = $(this).parent().siblings(".task_id").html();
+  const task = $(this)
+    .parent()
+    .siblings("strong")
+    .children(".task_content")
+    .html();
 
-    addAgendaUI(task, id);
-    updateConfirmAgendaFB(id, "", false);
-    updateTaskCount("Add");
-    $(this).parent().parent().parent().remove();
+  addAgendaUI(task, id);
+  updateConfirmAgendaFB(id, "", false);
+  updateTaskCount("Add");
+  $(this).parent().parent().parent().remove();
 });
 
 //Delete Agenda Modal
-const deleteAgendaModal = document.getElementById('deleteAgendaModal')
+const deleteAgendaModal = document.getElementById("deleteAgendaModal");
 if (deleteAgendaModal) {
-    deleteAgendaModal.addEventListener('show.bs.modal', event => {
+  deleteAgendaModal.addEventListener("show.bs.modal", (event) => {
     // Button that triggered the modal
     const button = event.relatedTarget;
     // Extract info from data-bs-* attributes
-    const id = button.getAttribute('data-bs-id');
-    const task = button.getAttribute('data-bs-task');
+    const id = button.getAttribute("data-bs-id");
+    const task = button.getAttribute("data-bs-task");
     // If necessary, you could initiate an Ajax request here
     // and then do the updating in a callback.
     console.log(id);
     console.log(task);
     // Update the modal's content.
-    const task_content = deleteAgendaModal.querySelector('.modal-body .task_content');
-    const task_id = deleteAgendaModal.querySelector('.modal-body .task_id');
+    const task_content = deleteAgendaModal.querySelector(
+      ".modal-body .task_content"
+    );
+    const task_id = deleteAgendaModal.querySelector(".modal-body .task_id");
 
     task_content.textContent = task;
     task_id.textContent = id;
-  })
+  });
 }
 
 var tasks = [];
@@ -91,26 +139,25 @@ var tasksCount = 0;
 //Task count
 
 function updateTaskCount(type = "Update") {
-    if (type == "Add") {
-        tasksCount++;
-    } else if (type == "Update") {
-        //do nothing
-    } else {
-        tasksCount--;
-    }
+  if (type == "Add") {
+    tasksCount++;
+  } else if (type == "Update") {
+    //do nothing
+  } else {
+    tasksCount--;
+  }
 
-    if (tasksCount > 1) {
-        $(".task_count").html(tasksCount.toString() + " tasks");
-    } else {
-        $(".task_count").html(tasksCount.toString() + " task");
-    }
-
+  if (tasksCount > 1) {
+    $(".task_count").html(tasksCount.toString() + " tasks");
+  } else {
+    $(".task_count").html(tasksCount.toString() + " task");
+  }
 }
 
 //Notifications
 
 function addCompletedNotification(id, task, time) {
-    $(".completion_alerts").append(`
+  $(".completion_alerts").append(`
         <div class="card bg-success-subtle text-success-emphasis border border-success-subtle">
                                         <div class="card-body">
                                           <i class="material-icons">task_alt</i> Completed task
@@ -127,47 +174,44 @@ function addCompletedNotification(id, task, time) {
 
 //Edit card functions
 function showEditCard(elem) {
-    $(elem).addClass("hide");
-    $(elem).siblings(".edit-card").removeClass("hide");
-    $("#addAgendaTextBox").focus();
+  $(elem).addClass("hide");
+  $(elem).siblings(".edit-card").removeClass("hide");
+  $("#addAgendaTextBox").focus();
 }
 
 function hideEditCard() {
-    $("#addAgendaTextBox").val("");
-    $(".btn_add_agenda").removeClass("hide");
-    $(".btn_add_agenda").siblings(".edit-card").addClass("hide");
+  $("#addAgendaTextBox").val("");
+  $(".btn_add_agenda").removeClass("hide");
+  $(".btn_add_agenda").siblings(".edit-card").addClass("hide");
 }
 
 function valueChangedNewAgenda(e, elem) {
-    const val = $(elem).val();
-    if (val != "") {
-        $("#btnAddAgenda").removeClass("disabled");
-    } else {
-        $("#btnAddAgenda").addClass("disabled");
-    }
-
+  const val = $(elem).val();
+  if (val != "") {
+    $("#btnAddAgenda").removeClass("disabled");
+  } else {
+    $("#btnAddAgenda").addClass("disabled");
+  }
 }
 
 function checkKeyNewAgenda(e, elem) {
-    if (e.key === 'Escape' || e.keyCode === 27) {
-        hideEditCard();
+  if (e.key === "Escape" || e.keyCode === 27) {
+    hideEditCard();
+  }
+  const task = $(elem).val();
+  if (task != "") {
+    if (e.key === "Enter" || e.keyCode === 13) {
+      const id = Date.now();
+      // console.log(id);
+      addAgendaFB(task, id);
+      addAgendaUI(task, id);
+      hideEditCard();
     }
-    const task = $(elem).val();
-    if (task != "") {
-        if (e.key === 'Enter' || e.keyCode === 13) {
-            const id = Date.now();
-            // console.log(id);
-            addAgendaFB(task, id);
-            addAgendaUI(task, id);
-            hideEditCard();
-        }
-    }
-
-
+  }
 }
 
 function addAgendaUI(task, id) {
-    $(".tasks").append(`
+  $(".tasks").append(`
         <div>
                                             <div class="card update-card hide">
                                                 <div class="card-body">
@@ -203,170 +247,194 @@ function addAgendaUI(task, id) {
         `);
 }
 
-
 //update card functions
 
 function checkKeyUpdateAgenda(e, elem) {
-    if (e.key === 'Escape' || e.keyCode === 27) {
-        $(elem).parent().parent().addClass("hide");
-        $(elem).parent().parent().siblings(".task").removeClass("hide");
+  if (e.key === "Escape" || e.keyCode === 27) {
+    $(elem).parent().parent().addClass("hide");
+    $(elem).parent().parent().siblings(".task").removeClass("hide");
+  }
+  const task = $(elem).val();
+  if (task != "") {
+    if (e.key === "Enter" || e.keyCode === 13) {
+      const id = $(elem)
+        .parent()
+        .parent()
+        .siblings(".task")
+        .children(".task_id")
+        .html();
+      updateAgendaFB(task, id);
+      $(elem)
+        .parent()
+        .parent()
+        .siblings(".task")
+        .children(".task_content")
+        .html(task);
+      $(elem).parent().parent().addClass("hide");
+      $(elem).parent().parent().siblings(".task").removeClass("hide");
     }
-    const task = $(elem).val();
-    if (task != "") {
-        if (e.key === 'Enter' || e.keyCode === 13) {
-            const id = $(elem).parent().parent().siblings(".task").children(".task_id").html();
-            updateAgendaFB(task, id);
-            $(elem).parent().parent().siblings(".task").children(".task_content").html(task);
-            $(elem).parent().parent().addClass("hide");
-            $(elem).parent().parent().siblings(".task").removeClass("hide");
-        }
-    }
-
+  }
 }
 
 function hideUpdateCard(elem) {
-    $(elem).parent().parent().parent().addClass("hide");
-    $(elem).parent().parent().parent().siblings(".task").removeClass("hide");
+  $(elem).parent().parent().parent().addClass("hide");
+  $(elem).parent().parent().parent().siblings(".task").removeClass("hide");
 }
 
 function valueChangedUpdateAgenda(elem) {
-    const val = $(elem).val();
-    if (val != "") {
-        $(".btn-update").removeClass("disabled");
-    } else {
-        $(".btn-update").addClass("disabled");
-    }
+  const val = $(elem).val();
+  if (val != "") {
+    $(".btn-update").removeClass("disabled");
+  } else {
+    $(".btn-update").addClass("disabled");
+  }
 }
 
 function updateAgenda(elem, id) {
-    const task = $(elem).parent().siblings(".input-task").val();
-    updateAgendaFB(task, id);
-    // console.log(val);
-    $(elem).parent().parent().parent().siblings(".task").children(".task_content").html(task);
-    $(elem).parent().parent().parent().addClass("hide");
-    $(elem).parent().parent().parent().siblings(".task").removeClass("hide");
-
+  const task = $(elem).parent().siblings(".input-task").val();
+  updateAgendaFB(task, id);
+  // console.log(val);
+  $(elem)
+    .parent()
+    .parent()
+    .parent()
+    .siblings(".task")
+    .children(".task_content")
+    .html(task);
+  $(elem).parent().parent().parent().addClass("hide");
+  $(elem).parent().parent().parent().siblings(".task").removeClass("hide");
 }
 
 //Firebase functions
 
 function loadAgendaFB() {
-    const data = {date : getToday()};
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/agenda/getAgendas");
-    xhttp.onload = function () {
-        const response = JSON.parse(this.responseText);
-        tasks = response;
-        loadAgendaList(response);
-        loadCompletedAgendas(response);
-        $(".loading_agenda").addClass("hide");
-        $(".tasks-container").removeClass("hide");
-
-    }
-    xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.send(JSON.stringify(data));
+  const data = { date: getToday() };
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "/agenda/getAgendas");
+  xhttp.onload = function () {
+    const response = JSON.parse(this.responseText);
+    tasks = response;
+    loadAgendaList(response);
+    loadCompletedAgendas(response);
+    $(".loading_agenda").addClass("hide");
+    $(".tasks-container").removeClass("hide");
+  };
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.send(JSON.stringify(data));
 }
 
 function loadAgendaList(data) {
-    var count = 0;
-    for (let i = 0; i < data.length; i++) {
-        if (!data[i].isCompleted) {
-            addAgendaUI(data[i].task, data[i].id);
-            count++;
-        }
+  var count = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (!data[i].isCompleted) {
+      addAgendaUI(data[i].task, data[i].id);
+      count++;
     }
-    tasksCount = count;
-    updateTaskCount();
-
+  }
+  tasksCount = count;
+  updateTaskCount();
 }
 
 function addAgendaFB(task, id) {
-    const data = { task: task, id: id, date : getToday() };
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/agenda/addAgenda");
-    xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.send(JSON.stringify(data));
-    updateTaskCount("Add");
+  const data = { task: task, id: id, date: getToday() };
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "/agenda/addAgenda");
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.send(JSON.stringify(data));
+  updateTaskCount("Add");
 }
 
 function updateAgendaFB(task, id) {
-    const data = { task: task, id: id, date : getToday()};
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/agenda/updateAgenda");
-    xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.send(JSON.stringify(data));
+  const data = { task: task, id: id, date: getToday() };
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "/agenda/updateAgenda");
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.send(JSON.stringify(data));
 }
 
 function deleteAgendaFB(id) {
-    const data = { id: id ,date : getToday()};
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/agenda/deleteAgenda");
-    xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.send(JSON.stringify(data));
+  const data = { id: id, date: getToday() };
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "/agenda/deleteAgenda");
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.send(JSON.stringify(data));
 }
 
 function updateConfirmAgendaFB(id, time, isCompleted = true) {
-    const data = { id: id, time: time, isCompleted: isCompleted , date : getToday()};
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/agenda/updateConfirmAgenda");
-    xhttp.setRequestHeader('Content-Type', 'application/json');
-    xhttp.send(JSON.stringify(data));
+  const data = {
+    id: id,
+    time: time,
+    isCompleted: isCompleted,
+    date: getToday(),
+  };
+  const xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "/agenda/updateConfirmAgenda");
+  xhttp.setRequestHeader("Content-Type", "application/json");
+  xhttp.send(JSON.stringify(data));
 }
 
 function loadCompletedAgendas(data) {
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].isCompleted) {
-            addCompletedNotification(data[i].id, data[i].task, data[i].time);
-        }
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].isCompleted) {
+      addCompletedNotification(data[i].id, data[i].task, data[i].time);
     }
+  }
 }
 
 function setToday() {
-    const date = new Date();
+  const date = new Date();
 
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
 
-    // This arrangement can be altered based on how we want the date's format to appear.
-    let currentDate = `${day}-${month}-${year}`;
-    $(".date").html(currentDate);
+  // This arrangement can be altered based on how we want the date's format to appear.
+  let currentDate = `${day}-${month}-${year}`;
+  $(".date").html(currentDate);
 }
 
-function getToday(){
-    const date = new Date();
+function getToday() {
+  const date = new Date();
 
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
 
-    // This arrangement can be altered based on how we want the date's format to appear.
-    let currentDate = `${day}-${month}-${year}`;
-    return currentDate;
+  // This arrangement can be altered based on how we want the date's format to appear.
+  let currentDate = `${day}-${month}-${year}`;
+  return currentDate;
 }
 
-function loadPlanRoster(){
-    $.ajax({
-        url: '/roster/getRoster',
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            generatePlanRosterUI(data);
-            $('.slots_plan').removeClass('hide');
-            $(".loading_roster_agenda").addClass("hide");
-        },
-        error: function (xhr, status, error) {
-            console.error('Error loading roster:', error);
-        }
-    });
+function getYesterday(dt) {
+  return moment(dt, "D-M-YYYY").subtract(1, "days").format("D-M-YYYY");
 }
 
-function generatePlanRosterUI(data){
-    $('#plan11AM').html(data[0].data["11AM"]);
-    $('#plan1PM').html(data[0].data["1PM"]);
-    $('#plan3PM').html(data[0].data["3PM"]);
-    $('#plan5PM').html(data[0].data["5PM"]);
-    $('#plan7PM').html(data[0].data["7PM"]);
+function getTommorrow(dt) {
+  return moment(dt, "D-M-YYYY").add(1, "days").format("D-M-YYYY");
+}
+
+function loadPlanRoster() {
+  $.ajax({
+    url: "/roster/getRoster",
+    type: "GET",
+    dataType: "json",
+    success: function (data) {
+      generatePlanRosterUI(data);
+      $(".slots_plan").removeClass("hide");
+      $(".loading_roster_agenda").addClass("hide");
+    },
+    error: function (xhr, status, error) {
+      console.error("Error loading roster:", error);
+    },
+  });
+}
+
+function generatePlanRosterUI(data) {
+  $("#plan11AM").html(data[0].data["11AM"]);
+  $("#plan1PM").html(data[0].data["1PM"]);
+  $("#plan3PM").html(data[0].data["3PM"]);
+  $("#plan5PM").html(data[0].data["5PM"]);
+  $("#plan7PM").html(data[0].data["7PM"]);
 }
 
 setToday();
